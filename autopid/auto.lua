247a1731b78c32bffdfd9bc8c480a0fcce1104b2
Super <superminor12@gmail.com>
2015-06-04 00:10:18 -0500

    .

--- autopid/auto.lua
+++ autopid/auto.lua
@@ -17,18 +17,12 @@ local function loadFile(file, cid, address, type)
     __newindex=controller,
   })
   --load and execute the file
-  if type == "br_turbine" then do 
-     controller.turbine = component.proxy(address)
-     controller.turbine.setActive(true) 
-  end
-  end
-  if type == "br_reactor" then do
-     controller.reactor = component.proxy(address)
-     controller.reactor.setActive(true) 
-  end
-  end
+  print("Injecting address: "..address)
+  controller.address = address
+
   assert(loadfile(file, "t",env))()
   --initialize the controller
+
   return pid.new(controller, cid, true)
 end
 
@@ -40,14 +34,14 @@ for address, type in component.list() do
 if type == "br_turbine" then do
   turbines = turbines + 1
   print("Detected turbine #"..tostring(turbines).." address: "..address)
-  loadFile("/pids/examples/turbine-example.pid", "turbine"..tostring(turbines), address, type)
+  loadFile("/usr/autopid/turbine.pid", "turbine"..tostring(turbines), address, type)
 end 
 end
 
 if type == "br_reactor" then do
   reactors = reactors + 1
   print("Detected reactor #"..tostring(reactors).." address: "..address)
-  loadFile("/pids/examples/reactor-example.pid", "reactor"..tostring(reactors), address, type)
+  loadFile("/usr/autopid/reactor.pid", "reactor"..tostring(reactors), address, type)
 end
 end
 

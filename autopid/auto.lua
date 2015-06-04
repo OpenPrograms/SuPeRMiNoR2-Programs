ee3342c825554a1f8805b497500c280e293e8567
Super <superminor12@gmail.com>
2015-06-04 00:32:40 -0500

    Fix that stupidness

--- autopid/auto.lua
+++ autopid/auto.lua
@@ -29,19 +29,15 @@ turbines = 0
 reactors = 0
 
 for address, type in component.list() do
+  if type == "br_turbine" then
+    turbines = turbines + 1
+    print("Detected turbine #"..tostring(turbines).." address: "..address)
+    loadFile("/usr/autopid/turbine.pid", "turbine"..tostring(turbines), address, type)
+  end 
 
-if type == "br_turbine" then do
-  turbines = turbines + 1
-  print("Detected turbine #"..tostring(turbines).." address: "..address)
-  loadFile("/usr/autopid/turbine.pid", "turbine"..tostring(turbines), address, type)
-end 
-end
-
-if type == "br_reactor" then do
-  reactors = reactors + 1
-  print("Detected reactor #"..tostring(reactors).." address: "..address)
-  loadFile("/usr/autopid/reactor.pid", "reactor"..tostring(reactors), address, type)
-end
-end
-
+  if type == "br_reactor" then
+    reactors = reactors + 1
+    print("Detected reactor #"..tostring(reactors).." address: "..address)
+    loadFile("/usr/autopid/reactor.pid", "reactor"..tostring(reactors), address, type)
+  end
 end

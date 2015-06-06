cb04a0abe948cc2e4089adabaf8ae818b94ce877
Super <superminor12@gmail.com>
2015-06-06 02:23:32 -0500

    Padding

--- power-monitor/power-monitor.lua
+++ power-monitor/power-monitor.lua
@@ -265,8 +265,8 @@ while true do
   for cid, cobj in pairs(controllers) do
     local status = cobj.status
     if cobj.type == "br_turbine" then
-      line = string.format("[%s] | Active: %s | Rotor Speed: %s | Power Gen: %s", pad(cid, 2), pad(status.active, 1), 
-        pad(round(status.rotorSpeed, 1), 1), pad(round(status.energyProduced, 1), 1))
+      line = string.format("%s | Active: %s | Rotor Speed: %s | Power Gen: %s", pad(cid, 9), pad(status.active, 1), 
+        pad(round(status.rotorSpeed, 1), 7), pad(round(status.energyProduced, 1), 1))
       buffer(line)
     end
   end

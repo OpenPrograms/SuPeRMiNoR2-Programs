e46cddd2b9a4146ed59457d625e84c567ff18f26
SuPeRMiNoR2 <superminor12@gmail.com>
2015-11-09 17:16:19 -0600

    Limit range of random data, and length

--- osmagdoor/osmag.lua
+++ osmagdoor/osmag.lua
@@ -1,10 +1,10 @@
 m = {}
 
 function m.makeCode()
-    local l = 32
+    local l = 10
     local s = ""
     for i = 1, l do
-        s = s .. string.char(math.random(32, 126))
+        s = s .. string.char(math.random(97, 122))
     end
     return s
 end

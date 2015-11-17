8a7c7cbbb502662930287c7111fd6c4eda7c2ecd
SuPeRMiNoR2 <superminor12@gmail.com>
2015-11-17 15:57:45 -0600

    I will work on this later

--- osmagdoor/doornode.lua
+++ osmagdoor/doornode.lua
@@ -1,1 +1,9 @@
 --And so it begins.
+component = require("component")
+ser = require("serialization")
+
+if component.isAvailable("modem") == false then
+  print("This program requires a network modem to be installed.")
+  break
+end
+

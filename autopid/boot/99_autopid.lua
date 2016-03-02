d71965005dae1da91d8a36731adab267a9a28853
SuPeRMiNoR2 <superminor12@gmail.com>
2016-03-02 00:32:50 -0600

    Wrapped whole boot script in pcall to avoid ever trapping computers in a crash

--- autopid/boot/99_autopid.lua
+++ autopid/boot/99_autopid.lua
@@ -1,23 +1,32 @@
-autopid = require("autopidlib")
-fs = require("filesystem")
+local function startup()
+  autopid = require("autopidlib")
+  fs = require("filesystem")
 
-configlocation = "/etc/autopid_autostart.txt"
+  configlocation = "/etc/autopid_autostart.txt"
 
-if fs.exists(configlocation) == false then
-  f = io.open(configlocation, "w")
-  f.write("false")
-  f.close()
+  if fs.exists(configlocation) == false then
+    f = io.open(configlocation, "w")
+    f.write("false")
+    f.close()
+  end
+
+  config = io.open(configlocation, "r")
+
+  status = config:read()
+  config:close()
+
+  if status == "true" then
+    print("[autopid] Scanning for machines...")
+    if pcall(autopid.scan) then
+      print("[autopid] Done.")
+    else
+      print("[autopid] crashed while scanning")
+    end
+  end
 end
 
-config = io.open(configlocation, "r")
-
-status = config:read()
-config:close()
-
-if status == "true" then 
-  if pcall(autopid.scan) then
-    print("[autopid] done")
-  else
-    print("[autopid] crashed while scanning")
-  end
+status, perror = pcall(startup())
+if status == false then
+  print("[autopid] Unknown error while running the startup script!")
+  print(perror)
 end
